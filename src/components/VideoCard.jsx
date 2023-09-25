eb7d0f58d22d09c813f3a6c96af26f17a1445f4f
Minjoo <m0522inju@gmail.com>
2023-09-25 19:24:48 +0900

    fix : a 태그 Link로 변경

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Channel from './Channel';
 import formatDate from './formatDate';
+import { Link } from 'react-router-dom';
 
 export default function VideoCard({ id, video }) {
   return (
     <li>
-      <a href={`/videos/watch/${id}`}>
+      <Link to={`/videos/watch/${id}`}>
         <img src={video.thumbnails.medium.url} alt='video thumbnail' className='w-full rounded-md' />
-      </a>
+      </Link>
       <div>
-        <a href={`/videos/watch/${id}`}>
+        <Link to={`/videos/watch/${id}`}>
           <h3 className='font-bold line-clamp-2' dangerouslySetInnerHTML={{ __html: video.title }} />
-        </a>
+        </Link>
       </div>
       <Channel id={video.channelId} />
       <span className='text-xs text-slate-500'>{formatDate(video.publishedAt)}</span>
